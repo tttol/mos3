52942a43d5ab3a660bd4c772ef7affea79226e23
t-takahashi <tttol.t4k.b1z@gmail.com>
2024-06-25 07:49:06 +0900

    initial commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module mos3
+
+go 1.21.3
---
+++ main.go
@@ -0,0 +1,151 @@
+package main
+
+import (
+	"fmt"
+	"html/template"
+	"io"
+	"net/http"
+	"os"
+	"path/filepath"
+)
+
+const uploadDir = "./uploads"
+
+func main() {
+	http.HandleFunc("/", indexHandler)
+	http.HandleFunc("/upload", uploadHandler)
+	http.HandleFunc("/delete", deleteHandler)
+	http.HandleFunc("/rename", renameHandler)
+	http.HandleFunc("/mkdir", mkdirHandler)
+	http.HandleFunc("/rmdir", rmdirHandler)
+	http.HandleFunc("/renamedir", renamedirHandler)
+
+	fmt.Println("Starting server at :3333")
+	http.ListenAndServe(":3333", nil)
+}
+
+func indexHandler(w http.ResponseWriter, r *http.Request) {
+	files, err := os.ReadDir(uploadDir)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	tmpl, err := template.ParseFiles("templates/index.html")
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	tmpl.Execute(w, files)
+}
+
+func uploadHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method != "POST" {
+		http.Error(w, "Invalid request method", http.StatusMethodNotAllowed)
+		return
+	}
+
+	file, header, err := r.FormFile("file")
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	defer file.Close()
+
+	dst, err := os.Create(filepath.Join(uploadDir, header.Filename))
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	defer dst.Close()
+
+	if _, err := io.Copy(dst, file); err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	http.Redirect(w, r, "/", http.StatusFound)
+}
+
+func deleteHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method != "POST" {
+		http.Error(w, "Invalid request method", http.StatusMethodNotAllowed)
+		return
+	}
+
+	filename := r.FormValue("filename")
+	err := os.Remove(filepath.Join(uploadDir, filename))
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	http.Redirect(w, r, "/", http.StatusFound)
+}
+
+func renameHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method != "POST" {
+		http.Error(w, "Invalid request method", http.StatusMethodNotAllowed)
+		return
+	}
+
+	oldFilename := r.FormValue("oldFilename")
+	newFilename := r.FormValue("newFilename")
+	err := os.Rename(filepath.Join(uploadDir, oldFilename), filepath.Join(uploadDir, newFilename))
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	http.Redirect(w, r, "/", http.StatusFound)
+}
+
+func mkdirHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method != "POST" {
+		http.Error(w, "Invalid request method", http.StatusMethodNotAllowed)
+		return
+	}
+
+	dirname := r.FormValue("dirname")
+	err := os.Mkdir(filepath.Join(uploadDir, dirname), os.ModePerm)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	http.Redirect(w, r, "/", http.StatusFound)
+}
+
+func rmdirHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method != "POST" {
+		http.Error(w, "Invalid request method", http.StatusMethodNotAllowed)
+		return
+	}
+
+	dirname := r.FormValue("dirname")
+	err := os.Remove(filepath.Join(uploadDir, dirname))
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	http.Redirect(w, r, "/", http.StatusFound)
+}
+
+func renamedirHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method != "POST" {
+		http.Error(w, "Invalid request method", http.StatusMethodNotAllowed)
+		return
+	}
+
+	oldDirname := r.FormValue("oldDirname")
+	newDirname := r.FormValue("newDirname")
+	err := os.Rename(filepath.Join(uploadDir, oldDirname), filepath.Join(uploadDir, newDirname))
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	http.Redirect(w, r, "/", http.StatusFound)
+}
