32555c6e29525357f6543c5de4318bb4ca9a595c
t-takahashi <tttol.t4k.b1z@gmail.com>
2024-06-29 17:32:47 +0900

    fix logging

--- core/logging/request_logging.go
+++ core/logging/request_logging.go
@@ -5,12 +5,17 @@ import (
 	"io"
 	"log/slog"
 	"net/http"
+	"strings"
 )
 
 func LogRequest(r *http.Request) {
 	slog.Info("[Request URL&method]", "url", r.URL.String(), "method", r.Method)
 	for name, headers := range r.Header {
 		for _, h := range headers {
+			if strings.EqualFold(name, "Authorization") {
+				// skip Authorization header
+				continue
+			}
 			slog.Info("[Request Header]", name, h)
 		}
 	}
