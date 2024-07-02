cdf242459ccbfead09b60b389f8975699bf8a746
t-takahashi <tttol.t4k.b1z@gmail.com>
2024-07-03 08:54:08 +0900

    fix typo

--- Dockerfile
+++ Dockerfile
@@ -3,7 +3,7 @@ FROM golang:latest
 WORKDIR /app
 COPY go.mod .
 # COPY go.sum .
-RUN go mod downloadjjj
+RUN go mod download
 COPY . .
 
 CMD ["go", "run", "main.go"]
--- release.sh
+++ release.sh
@@ -3,7 +3,7 @@ if [ $# != 1 ]; then
     echo "example: ./release.sh 1.0"
     exit 1
 fi
-
+docker build -t mos3:$1 .
 docker tag mos3:$1 tttol/mos3:$1
 docker tag mos3:$1 tttol/mos3:latest
 docker push tttol/mos3:$1
