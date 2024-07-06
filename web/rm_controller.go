c2b911a07fe40dff751994ab6c630e53d8a8eba8
t-takahashi <tttol.t4k.b1z@gmail.com>
2024-07-06 10:04:29 +0900

    add remove logic

---
+++ web/rm_controller.go
@@ -0,0 +1,27 @@
+package web
+
+import (
+	"log/slog"
+	"net/http"
+	"os"
+	"path/filepath"
+)
+
+func RemoveHandler(w http.ResponseWriter, r *http.Request) {
+	slog.Info("RemoveHandler is called.")
+	if r.Method != "POST" {
+		http.Error(w, "Invalid request method", http.StatusMethodNotAllowed)
+		return
+	}
+
+	path := r.FormValue("path")[len("/s3/"):]
+	err := os.Remove(filepath.Join(UPLOAD_DIR, path))
+	if err != nil {
+		slog.Error("Failed to remove.", "error", err, "path", path)
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	slog.Info("Remove file success.", "path", path)
+
+	http.Redirect(w, r, "/s3", http.StatusFound)
+}
