dfcdc10eb303fb8f2cfe8447a58a35216d448282
t-takahashi <tttol.t4k.b1z@gmail.com>
2024-07-06 19:03:13 +0900

    fix breadcrumbs.go

--- core/util/breadcrumbs_test.go
+++ core/util/breadcrumbs_test.go
@@ -8,29 +8,40 @@ import (
 func TestGenerateBreadcrumbs(t *testing.T) {
 	tests := []struct {
 		input    string
-		expected map[string]interface{}
+		expected []Breadcrumb
 	}{
 		{
 			input: "/fuga/fugafuga/aaa.txt",
-			expected: map[string]interface{}{
-				"fuga":     "/s3/fuga",
-				"fugafuga": "/s3/fuga/fugafuga",
-				"aaa.txt":  "/s3/fuga/fugafuga/aaa.txt?action=dl",
+			expected: []Breadcrumb{
+				{Name: "fuga", Path: "/s3/fuga"},
+				{Name: "fugafuga", Path: "/s3/fuga/fugafuga"},
+				{Name: "aaa.txt", Path: "/s3/fuga/fugafuga/aaa.txt?action=dl"},
 			},
 		},
 		{
 			input: "/hoge/fuga/piyo",
-			expected: map[string]interface{}{
-				"hoge": "/s3/hoge",
-				"fuga": "/s3/hoge/fuga",
-				"piyo": "/s3/hoge/fuga/piyo",
+			expected: []Breadcrumb{
+				{Name: "hoge", Path: "/s3/hoge"},
+				{Name: "fuga", Path: "/s3/hoge/fuga"},
+				{Name: "piyo", Path: "/s3/hoge/fuga/piyo"},
 			},
 		},
 		{
 			input: "/s3/hoge",
-			expected: map[string]interface{}{
-				"s3":   "/s3/s3",
-				"hoge": "/s3/s3/hoge",
+			expected: []Breadcrumb{
+				{Name: "s3", Path: "/s3/s3"},
+				{Name: "hoge", Path: "/s3/s3/hoge"},
+			},
+		},
+		{
+			input: "/files/2024/0706/1845/txt/1234.txt",
+			expected: []Breadcrumb{
+				{Name: "files", Path: "/s3/files"},
+				{Name: "2024", Path: "/s3/files/2024"},
+				{Name: "0706", Path: "/s3/files/2024/0706"},
+				{Name: "1845", Path: "/s3/files/2024/0706/1845"},
+				{Name: "txt", Path: "/s3/files/2024/0706/1845/txt"},
+				{Name: "1234.txt", Path: "/s3/files/2024/0706/1845/txt/1234.txt?action=dl"},
 			},
 		},
 	}
